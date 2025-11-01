55a2199051aa920fb42a3e7df0e8d43d9532ed4c
alangonzalez91 <alangonzalez91@gmail.com>
2025-11-01 00:51:50 -0300

    prueba modificacion tailwind

--- src/componentes/navbar.jsx
+++ src/componentes/navbar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-    <nav className="bg-gray-950 text-white shadow-md fixed top-0 left-0 w-full z-50">
+    <nav className="bg-gray-800 text-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="max-w-7xl mx-auto px-6 py-3 flex justify-between items-center">
         {/* Logo o nombre */}
         <h1 className="text-2xl font-bold">MiSitio</h1>
