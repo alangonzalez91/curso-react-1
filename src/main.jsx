c729fb639f5ccc3b6932ad29841802da93c12534
null <null>
2025-11-20 16:23:46 -0300

    se aplico useQuery

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css'; /// Tellwind aplicado 2025-10-30
 import App from './App.jsx';
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>  
+  ,
 )
