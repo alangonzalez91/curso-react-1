3d8ae5a6b517d985db8ae887d9a1c9f10f8da716
alangonzalez91 <alangonzalez91@gmail.com>
2025-12-13 16:10:22 -0300

    cambios visuales

--- src/componentes/calendarioMensual.jsx
+++ src/componentes/calendarioMensual.jsx
@@ -44,7 +44,7 @@ function CalendarioMensual() {
       <h2 className="flex align-center">
         <button className="pr-3 text-2xl" onClick={mesAnterior}>◀</button>
         <h2 className="text-2xl" style={{ width: "480px" }}>{nombreMes.charAt(0).toUpperCase() + nombreMes.slice(1)} {año}</h2>
-        <button className="pl-3 text-2xl" style={{ position: "fixed", left: "860px" }} onClick={mesSiguiente}>▶</button>
+        <button className="pl-3 text-2xl" style={{  left: "860px" }} onClick={mesSiguiente}>▶</button>
       </h2>
 
       <div
